27ed442f5202a526f1cfda88919801e99a611aab
prrtek <prrtek@outlook.com>
2023-06-05 11:15:42 +0530

    better

--- src/Day1/setMatrixZero.java
+++ src/Day1/setMatrixZero.java
@@ -36,5 +36,35 @@ public class setMatrixZero {
             }
         }
     }
+
+
+    int row = matrix.length; // number of rows
+    int column = matrix[0].length; // number of columns
+
+    int[] rowArr = new int[row]; // row array
+    int[] colArr = new int[column]; // column array
+
+    // Traverse the matrix:
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of rowArr with 1:
+                rowArr[i] = 1;
+
+                // mark jth index of colArr with 1:
+                colArr[j] = 1;
+            }
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if rowArr[i] or colArr[j] is marked with 1.
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (rowArr[i] == 1 || colArr[j] == 1) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
     
 
