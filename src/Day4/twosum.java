7e1e021fa6c5eda8310cdb3fbeeb19f4571520e0
prrtek <prrtek@outlook.com>
2023-06-22 13:01:20 +0530

    two sum

---
+++ src/Day4/twosum.java
@@ -0,0 +1,18 @@
+package Day4;
+
+import java.util.HashMap;
+
+public class twosum {
+      public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int result=target-nums[i];
+            if(map.containsKey(result)){
+                return new int[]{i,map.get(result)};
+            }else{
+              map.put(nums[i],i);
+            }
+        }
+        return new int[0];
+    }
+}
